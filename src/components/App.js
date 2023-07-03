f76467fca073a50305dca6b48e93f7f8a63917b3
BhushanSasulkar <125477232+BhushanSasulkar@users.noreply.github.com>
2023-07-04 02:24:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
-  
-  return (
-    <div id="main">
+import React, { useState } from 'react';
 
+function MarcoPolo() {
+  const [displayText, setDisplayText] = useState('Marco');
+  const [buttonText, setButtonText] = useState('Polo');
+
+  const toggleText = () => {
+    if (displayText === 'Marco') {
+      setDisplayText('Polo');
+      setButtonText('Marco');
+    } else {
+      setDisplayText('Marco');
+      setButtonText('Polo');
+    }
+  };
+
+  return (
+    <div>
+      <h1 id="marco-polo">{displayText}</h1>
+      <button id="marco-polo-toggler" onClick={toggleText}>
+        {buttonText}
+      </button>
     </div>
-  )
+  );
 }
 
-
-export default App;
+export default MarcoPolo;
